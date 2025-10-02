6d6f7ac9cd5144d11c1f456eeed4e8e23bdf9492
Ales Wirt <awirt@aras.com>
2025-10-02 13:03:08 +0200

    Challenge #4

--- 11-arrays-bankist-app/script.js
+++ 11-arrays-bankist-app/script.js
@@ -219,12 +219,134 @@ btnLoan.addEventListener('click', e => {
 //   const balance = movements.reduce((acc, cur) => acc + cur, 0);
 //   labelBalance.textContent = `${balance}€`;
 // };
+// const calcDisplaySummary = movements => {
+//   const incomes = movements
+//     .filter(mov => mov > 0)
+//     .reduce((acc, cur) => acc + cur, 0);
+//   labelSumIn.textContent = `${incomes}€`;
 
+//   const outcome = movements
+//     .filter(mov => mov < 0)
+//     .reduce((acc, cur) => acc + cur, 0);
+//   labelSumOut.textContent = `${Math.abs(outcome)}€`;
+
+//   const interest = movements
+//     .filter(mov => mov > 0)
+//     .map(deposit => (deposit * 1.2) / 100)
+//     .filter((int, i, arr) => int >= 1)
+//     .reduce((acc, cur) => acc + cur, 0);
+
+//   labelSumInterest.textContent = `${interest}€`;
+// };
 // calcDisplaySummary(account1.movements);
 // const user = 'Steven Thomas Williams';
+// const createUserNames = accounts =>
+//   accounts.forEach(
+//     account =>
+//       (account.userName = account.owner
+//         .toLowerCase()
+//         .split(' ')
+//         .map(name => name[0])
+//         .join(''))
+//   );
+// createUserNames(accounts);
 
 ///////////////////////////////////////
-// Coding Challenge #2 and #3
+// Coding Challenge #4
+
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+// const breeds = [
+//   {
+//     breed: 'German Shepherd',
+//     averageWeight: 32,
+//     activities: ['fetch', 'swimming'],
+//   },
+//   {
+//     breed: 'Dalmatian',
+//     averageWeight: 24,
+//     activities: ['running', 'fetch', 'agility'],
+//   },
+//   {
+//     breed: 'Labrador',
+//     averageWeight: 28,
+//     activities: ['swimming', 'fetch'],
+//   },
+//   {
+//     breed: 'Beagle',
+//     averageWeight: 12,
+//     activities: ['digging', 'fetch'],
+//   },
+//   {
+//     breed: 'Husky',
+//     averageWeight: 26,
+//     activities: ['running', 'agility', 'swimming'],
+//   },
+//   {
+//     breed: 'Bulldog',
+//     averageWeight: 36,
+//     activities: ['sleeping'],
+//   },
+//   {
+//     breed: 'Poodle',
+//     averageWeight: 18,
+//     activities: ['agility', 'fetch'],
+//   },
+// ];
+
+// // 1.
+// const huskyWeight = breeds.find(b => b.breed === 'Husky').averageWeight;
+// console.log(huskyWeight);
+
+// // 2.
+// const dogsRunningFetchActivities = breeds.filter(
+//   b => b.activities.includes('running') && b.activities.includes('fetch')
+// );
+// console.log(dogsRunningFetchActivities);
+
+// // 3.
+// const allActivities = breeds.map(b => b.activities).flat();
+// console.log(allActivities);
+
+// // 4.
+// const uniqueActivities = new Set(breeds.map(b => b.activities).flat());
+// console.log(uniqueActivities);
+
+// // 5.
+// const swimmingAdjacent = [
+//   ...new Set(
+//     breeds
+//       .filter(b => b.activities.includes('swimming'))
+//       .flatMap(b => b.activities)
+//   ),
+// ];
+// console.log(swimmingAdjacent);
+
+// // 6.
+// const avgMore10kg = breeds.every(b => b.averageWeight >= 10);
+// console.log(avgMore10kg);
+
+// // 7.
+// const someActiveDogs = breeds.some(b => b.activities.length > 2);
+// console.log(someActiveDogs);
+
+///////////////////////////////////////
+// Coding Challenge #2
 
 /* 
 Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
